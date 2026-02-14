e1c9781ff6c5bb1e69dd352b3f32db8f16d49111
Александр Птицын <it.ptitsa@gmail.com>
2026-02-14 14:35:46 +0700

    feat: update welcome message text

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export function getWelcomeMessage() {
   const FEATURE_FLAG = true;
 
   if (FEATURE_FLAG) {
-    return "New feature enabled";
+    return "New feature is enabled v1";
   }
 
   return "Old version";
