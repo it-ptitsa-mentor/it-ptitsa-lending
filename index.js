37d9199c19f7e83720e6345a6e3d44675f5f5e97
Александр Птицын <it.ptitsa@gmail.com>
2026-02-14 14:36:56 +0700

    chore: update flag logic

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 export function getWelcomeMessage() {
   const FEATURE_FLAG = true;
 
-  console.log("FEATURE_FLAG", FEATURE_FLAG);
-
   if (FEATURE_FLAG) {
-    return "New feature is enabled v1";
+    return undefined;
   }
 
   return "Old version";
