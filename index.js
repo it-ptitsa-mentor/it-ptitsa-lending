7b6f21270e4f56c3ac33882493de698dd739c12e
Aleksandr Ptitsyn <102740066+it-ptitsa@users.noreply.github.com>
2026-02-16 06:15:54 +0300

    Changed discount to 7%

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export function calculateDiscount(price, userType) {
   console.log(`Calculating discount for ${price} and ${userType}`);
 
   if (userType === "regular") {
-    return price * 0.05;
+    return price * 0.07;
   }
 
   if (userType === "vip") {
