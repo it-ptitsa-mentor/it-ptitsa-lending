e94860e7c97261579c5c90c9977515b1f20aa817
Александр Птицын <it.ptitsa@gmail.com>
2026-02-16 08:49:08 +0700

    refactor: simply discount function

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 export function calculateDiscount(price, userType) {
+  console.log(`Calculating discount for ${price} and ${userType}`);
+
   if (userType === "regular") {
-    console.log(`Regular user ${price} discount is 5%`);
     return price * 0.05;
   }
 
   if (userType === "vip") {
-    console.log(`VIP user ${price} discount is 15%`);
     return price * 0.15;
   }
 
-  console.log(`Unknown user ${price} discount is 0%`);
   return 0;
 }
